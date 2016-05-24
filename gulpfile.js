a758cd5f014cc37d2a67a515101ae61b41ff332c
Nathan Reginato <nathan.reginato@gmail.com>
2016-05-24 17:56:02 -0600

    Initial

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sh = require('shelljs');
+var order = require('gulp-order');
+var paths = {
+  sass: ['./scss/*.scss'],
+  javascript: ['./project/**/*.js']
+};
+
+gulp.task('default', ['sass', 'js']);
+
+var javascriptFiles = [
+  'js/someFile.js'
+]
+
+gulp.task('js', function() {
+  gulp.src(['./project/**/*.js'])
+  .pipe(order([
+    'project/app.js',
+    ], { base: './' }))
+        .pipe(concat('scripts.min.js'))
+  .pipe(gulp.dest('./www/js')).on('error', gutil.log)
+});
+
+gulp.task('sass', function(done) {
+  gulp.src('./scss/*.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./www/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./www/css/')).on('error', gutil.log)
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.javascript, ['js']);
+});
+
+
+gulp.task('install', ['git-check'], function() {
+  return bower.commands.install()
+    .on('log', function(data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    });
+});
+
+gulp.task('git-check', function(done) {
+  if (!sh.which('git')) {
+    console.log(
+      '  ' + gutil.colors.red('Git is not installed.'),
+      '\n  Git, the version control system, is required to download Ionic.',
+      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+    );
+    process.exit(1);
+  }
+  done();
+});
---
+++ project/app.js
@@ -0,0 +1,29 @@
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('landing', {
+    url: '/',
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/');
+});
