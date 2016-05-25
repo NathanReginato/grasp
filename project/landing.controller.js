b178bfbf87a91b06c96dfa429437c7733b4c7f24
Nathan Reginato <nathan.reginato@gmail.com>
2016-05-25 11:26:19 -0600

    Server side error handling for login working

--- project/landing.controller.js
+++ project/landing.controller.js
@@ -1,9 +1,8 @@
 function landingController($http, $state, $localStorage, landingPage){
     var vm = this;
-    vm.errors = []
     console.log("controller connected");
     vm.signup = function(){
-      vm.errors = []
+      vm.signup.errors = []
       var obj = {
         name: vm.signup.username,
         password: vm.signup.password,
@@ -16,16 +15,25 @@ function landingController($http, $state, $localStorage, landingPage){
       }).catch(function(res){
       console.log('this is the catch');
         res.data.errors.forEach(function(elem){
-          vm.errors.push(elem)
+          vm.signup.errors.push(elem)
         })
       })
     }
 
     vm.login = function(){
+      vm.login.errors = []
       var obj = {
         password: vm.login.password,
         email: vm.login.email
       }
-      landingPage.logIn(obj)
+      landingPage.logIn(obj).then(function (res) {
+        console.log(res);
+        $localStorage.$default({ token: res.data.token })
+        $state.go('dashboard');
+      }).catch(function(res){
+        res.data.errors.forEach(function(elem){
+          vm.login.errors.push(elem)
+        })
+      })
     }
   }
--- project/landingpage.factory.js
+++ project/landingpage.factory.js
@@ -16,10 +16,11 @@ angular.module('graspMobile')
   // 'https://panic-button-g20.herokuapp.com/api/v1/auth/signup'
 
   function logIn(obj){
-    $http.post('https://panic-button-g20.herokuapp.com/api/v1/auth/login', obj).then(function(res){
-      console.log(res);
-      $localStorage.$default({ token: res.data.token })
-      $state.go('dashboard');
+    return $http.post('http://localhost:3000/api/v1/auth/login', obj)
+    .then(function(res){
+      return res
     });
   }
+
+  // 'https://panic-button-g20.herokuapp.com/api/v1/auth/login'
 });
