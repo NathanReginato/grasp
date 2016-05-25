708c0352af16f133b15bb9a3e421aad70d701561
Nathan Reginato <nathan.reginato@gmail.com>
2016-05-25 16:34:33 -0600

    Api calls to Heroku

--- project/dashboard.factory.js
+++ project/dashboard.factory.js
@@ -8,7 +8,7 @@ angular.module('graspMobile')
 
   function getClasses(obj){
     console.log('calling egte');
-    return $http.get('http://localhost:3000/api/v1/users/participations')
+    return $http.get('https://panic-button-g20.herokuapp.com/api/v1/users/participations')
     .then(function(res){
       console.log(res);
       return res
