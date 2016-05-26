c664217c97a6bc5f7d839f916fb523583b95a071
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-05-25 19:14:01 -0600

    changed api calls to the deployed heroku site

--- project/landingpage.factory.js
+++ project/landingpage.factory.js
@@ -8,7 +8,7 @@ angular.module('graspMobile')
 
   function signUp(obj){
     // return $http.post('https://panic-button-g20.herokuapp.com/api/v1/auth/signup', obj)
-    return $http.post('http://localhost:3000/api/v1/auth/signup', obj)
+    return $http.post('http://panic-button-g20.herokuapp.com/api/v1/auth/signup', obj)
     .then(function(res){
       return res
     });
@@ -18,7 +18,7 @@ angular.module('graspMobile')
 
   function logIn(obj){
     // return $http.post('https://panic-button-g20.herokuapp.com/api/v1/auth/login', obj)
-    return $http.post('http://localhost:3000/api/v1/auth/login', obj)
+    return $http.post('http://panic-button-g20.herokuapp.com/api/v1/auth/login', obj)
     .then(function(res){
       return res
     });
