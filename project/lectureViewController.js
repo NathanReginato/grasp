0c6e3b9942c3ddf5b340abf8faff46a0c4cafbd2
Nathan Reginato <nathan.reginato@gmail.com>
2016-05-26 13:15:59 -0600

    Slider added

--- project/lectureViewController.js
+++ project/lectureViewController.js
@@ -1,13 +1,15 @@
 function lectureViewController ($stateParams, landingPage){
     var socket = io.connect('http://Nick-MacBook-Air.local:3000');
-    socket.emit('set', {lecture_id: $stateParams.lecture_id, user_id:1})
     var lecture_id = 1; //$state.params.id
-    var status = 0;
+    var status = 2;
     var vm = this;
+
+    socket.emit('set', {lecture_id: $stateParams.lecture_id, user_id:landingPage.idGetter(), status_id: status})
     socket.on(lecture_id, function (data) {
       console.log(data);
     })
-    vm.vote = function(vote) {
+    vm.vote = function() {
+      var vote = parseInt(vm.slider)
       socket.emit('chart', {lecture_id: $stateParams.lecture_id, user_id: landingPage.idGetter(), status_id: vote, lastStatus: status})
       console.log({lecture_id: $stateParams.lecture_id, user_id: landingPage.idGetter(), status_id: vote, lastStatus: status});
       status = vote;
