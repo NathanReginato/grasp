a70a38c78d1e1f6b03f826367e3d2af68c3e85ae
Nathan Reginato <nathan.reginato@gmail.com>
2016-05-27 00:54:03 -0600

    Added dots

--- project/router.js
+++ project/router.js
@@ -40,5 +40,5 @@ angular.module('graspMobile')
   })
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise('/lectures');
 });
